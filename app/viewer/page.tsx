d982f34b39103144b4289081e125743e51993690
anibalinbalin <anibalin@gmail.com>
2026-04-20 16:14:58 -0300

    Remove legacy viewer loading fallback

--- app/viewer/page.tsx
+++ app/viewer/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 import { ViewerLoadingSkeleton } from "@/components/viewer-loading-skeleton";
 
 const ViewerV2 = dynamic(
@@ -13,9 +12,5 @@ const ViewerV2 = dynamic(
 );
 
 export default function ViewerV2Page() {
-  return (
-    <Suspense fallback={<ViewerLoadingSkeleton />}>
-      <ViewerV2 />
-    </Suspense>
-  );
+  return <ViewerV2 />;
 }
